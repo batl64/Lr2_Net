17c3c525ba6461372672348a72d9f89d62273d7b
batl64 <91633690+batl64@users.noreply.github.com>
2024-05-26 23:14:58 +0300

    Add files via upload

---
+++ Prototype/Prototype/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+// Прототип
+abstract class Prototype
+{
+    public abstract Prototype Clone();
+}
+
+// Конкретний прототип
+class ConcretePrototype : Prototype
+{
+    private string _name;
+
+    public ConcretePrototype(string name)
+    {
+        _name = name;
+    }
+
+    public override Prototype Clone()
+    {
+        return new ConcretePrototype(_name);
+    }
+
+    public void SetName(string name)
+    {
+        _name = name;
+    }
+
+    public void Show()
+    {
+        Console.WriteLine($"Name: {_name}");
+    }
+}
+
+// Клієнт
+class Client
+{
+    private Prototype _prototype;
+
+    public Client(Prototype prototype)
+    {
+        _prototype = prototype;
+    }
+
+    public Prototype Operation()
+    {
+        return _prototype.Clone();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Створення прототипу
+        ConcretePrototype prototype = new ConcretePrototype("Prototype");
+
+        // Клонування прототипу
+        ConcretePrototype clone1 = (ConcretePrototype)prototype.Clone();
+        clone1.Show(); // Виведе "Name: Prototype"
+
+        // Модифікація клонованого об'єкта
+        clone1.SetName("Modified Prototype");
+        clone1.Show(); // Виведе "Name: Modified Prototype"
+
+        // Клонування прототипу через клієнт
+        Client client = new Client(prototype);
+        ConcretePrototype clone2 = (ConcretePrototype)client.Operation();
+        clone2.Show(); // Виведе "Name: Prototype"
+    }
+}
